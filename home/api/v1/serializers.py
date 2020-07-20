dcbd9682f5fa6c998acec67d4db3c8926de5fcf0
Mayank Kushal <mayankkushal26@gmail.com>
2020-07-20 18:53:25 +0530

    add auth apis

--- home/api/v1/serializers.py
+++ home/api/v1/serializers.py
@@ -1,13 +1,22 @@
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.http import HttpRequest
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.utils.http import urlsafe_base64_encode as uid_encode
+from django.contrib.auth.tokens import default_token_generator
+
 from allauth.account import app_settings as allauth_settings
 from allauth.account.forms import ResetPasswordForm
 from allauth.utils import email_address_exists, generate_unique_username
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from rest_framework import serializers
-from rest_auth.serializers import PasswordResetSerializer
+from rest_auth.serializers import PasswordResetSerializer, PasswordResetConfirmSerializer
+from allauth.account.utils import user_pk_to_url_str
+from allauth.utils import build_absolute_uri
+
 
 from home.models import CustomText, HomePage
 
@@ -17,7 +26,7 @@ User = get_user_model()
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'email', 'password', 'role', 'organisation')
         extra_kwargs = {
             'password': {
                 'write_only': True,
@@ -46,11 +55,18 @@ class SignupSerializer(serializers.ModelSerializer):
         return email
 
     def create(self, validated_data):
+        first_name = validated_data.get('first_name')
+        last_name = validated_data.get('last_name')
+        name = f"{first_name} {last_name}"
         user = User(
             email=validated_data.get('email'),
-            name=validated_data.get('name'),
+            name=name,
+            first_name=first_name,
+            last_name=last_name,
+            role=validated_data.get('role'),
+            organisation=validated_data.get('organisation'),
             username=generate_unique_username([
-                validated_data.get('name'),
+                name,
                 validated_data.get('email'),
                 'user'
             ])
@@ -81,9 +97,39 @@ class HomePageSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'name']
+        fields = ['id', 'email', 'name', 'first_name', 'last_name', 'role', 'organisation']
 
+
+class CustomResetPasswordForm(ResetPasswordForm):
+    def save(self, request, **kwargs):
+        current_site = get_current_site(request)
+        email = self.cleaned_data["email"]
+        token_generator = kwargs.get("token_generator",
+                                     default_token_generator)
+
+        for user in self.users:
+            temp_key = token_generator.make_token(user)
+
+            # save it to the password reset model
+            # password_reset = PasswordReset(user=user, temp_key=temp_key)
+            # password_reset.save()
+
+            # send the password reset email
+            path = f"password/reset/key/{uid_encode(str(user.id).encode('ascii'))}/{temp_key}"
+
+            url = f"{settings.FE_URL}/{path}"
+
+            context = {"current_site": current_site,
+                       "user": user,
+                       "password_reset_url": url,
+                       "request": request}
+
+            get_adapter(request).send_mail(
+                'account/email/password_reset_key',
+                email,
+                context)
+        return self.cleaned_data["email"]
 
 class PasswordSerializer(PasswordResetSerializer):
     """Custom serializer for rest_auth to solve reset password error"""
-    password_reset_form_class = ResetPasswordForm
+    password_reset_form_class = CustomResetPasswordForm
